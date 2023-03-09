4554699544a8f57d097a3716d56585d90502b047
[ninano05] <[ohjy0530@duksung.ac.kr]>
2023-03-09 10:17:45 +0900

    3월 9일 수업 내용

---
+++ src/ch01/practice/Book.java
@@ -0,0 +1,16 @@
+package ch01.practice;
+
+public class Book {
+  //속성
+  private String name = "";
+
+  
+  //메소드
+  public Book(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
