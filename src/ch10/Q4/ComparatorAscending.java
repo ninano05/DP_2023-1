74d03875701884973e32815989086110f94ceca0
[ninano05] <[ohjy0530@duksung.ac.kr]>
2023-04-17 23:38:38 +0900

    4/13 연습문제 10-4까지 종료

--- src/ch10/Q4/ComparatorAscending.java
+++ src/ch10/Q4/ComparatorAscending.java
@@ -3,13 +3,17 @@ package ch10.Q4;
 import java.util.Comparator;
 
 public class ComparatorAscending implements Comparator {
+// Comparator 인터페이스를 구현해야 클래스 가능
 
+// 두개의 크기 비교시 사용하는 메서드가 compare 메서드이다.
   @Override
-  public int compare(Object o1, Object o2) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'compare'");
+  public int compare(Object a, Object b) {
+    //원칙: 정렬주체(List)는 음수가 리턴되면 그대로 두고 양수가 리턴되면 위치를 서로 바꾼다.
+    return ((String)a).compareTo((String)b); //String 클래스의 compareTo()를 이용해서 크기 비교를 함.
+    // (a - b)를 실행
+    // a가 작을 때는 음수가 리턴됨, a가 클 때는 양수가 리턴 됨.
+    // a가 클 때 위치를 서로 바꾼다, => 큰 걸 뒤로 보낸다 => 오름차순
   }
   
 
-  }
 }
--- src/ch10/Q4/Main.java
+++ src/ch10/Q4/Main.java
@@ -8,7 +8,9 @@ class Main {
         List<String> list = Arrays.asList("D", "B", "C", "E", "A");
 
         // 사전순으로 작은 순서 (오름차순)
-        list.sort( new ComparatorAscending() );
+        // list: 전략을 이용하는 컨텍스트
+        // Comparator: 전략 객체
+        list.sort( new ComparatorAscending() ); //인자가 콤패러터 어떻게 정렬할지 결정해줌.
         System.out.println(list);
 
         // 사전순으로 큰 순서 (내림차순)
